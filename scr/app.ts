c4140865e1a47826f603104302ee9b6271295cea
David Roa <david_roa_06@hotmail.com>
2024-08-16 07:51:46 -0500

    Connected to NorthWind database

--- scr/app.ts
+++ scr/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import pool from "./database/db_connect"
 require ('dotenv').config();
 
 const app = express();
 const port = process.env.PORT
 
-app.get('/', (req,res) => {s
-
+app.get('/', async (req,res) => {
+    const query = 'select * from employees';
+    const response = await pool.query(query);
+    console.log(response);
     res.send('hola Mundo El Servidor Esta Escuchando en el Puerto 3000')
 });
 
