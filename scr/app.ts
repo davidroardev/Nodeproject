28fc421be15d10161b57b7afe507ec1a085fafcf
David Roa <david_roa_06@hotmail.com>
2024-08-21 21:18:17 -0500

    added error Handler

--- scr/app.ts
+++ scr/app.ts
@@ -2,6 +2,7 @@ import express, { Router } from "express";
 import { getCategories, getCategoriesById, createCategories, deleteCategories, updateCategory } from "./controllers/categories_controller";
 import {generateToken} from "./controllers/user_controllers";
 import {authenticateToken} from "./middleware/authorization";
+import { errorHandler } from "./middleware/error";
 require ('dotenv').config();
 
 const app = express();
@@ -20,6 +21,7 @@ userRoutes.post('/api/login', generateToken);
 
 
 app.use(express.json());
+app.use(errorHandler);
 app.use(categoriesRoutes);
 app.use(userRoutes);
 
