95730cc1fa906641575f4fe4b44653668edb1c43
David Roa <david_roa_06@hotmail.com>
2024-08-26 17:57:37 -0500

    created user table and modify auth

--- scr/controllers/user_controllers.ts
+++ scr/controllers/user_controllers.ts
@@ -1,12 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import pool from "../database/db_connect";
 
 require('dotenv').config();
 
-export const generateToken = (req: Request, res: Response): Response =>{
+export const generateToken = async (req: Request, res: Response): Promise<Response> =>{
     const userName= req.body.username;
-    const user = {name: userName};
-    const accessToken = jwt.sign(user, `${process.env.CLAVE_JWT}`, {expiresIn: '1h'})
-    return res.status(200).json({accessToken})
+    const password = req.body.password;
+    const query = await pool.query('SELECT * FROM users WHERE user_name = $1 AND password = $2'
+        , [userName, password ]);
+    const user = query.rows[0];
+    if (query.rowCount !== null && query.rowCount > 0){
+        const accessToken = jwt.sign(user, `${process.env.CLAVE_JWT}`, {expiresIn: '1h'})
+        return res.status(200).json({accessToken})
+    }else {
+        return res.status(400).json('User not found')
+    }
+    
 };
 
