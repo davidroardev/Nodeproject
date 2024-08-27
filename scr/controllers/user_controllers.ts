5046a23f10fc97c1a5db5308cffdfc631293b723
David Roa <david_roa_06@hotmail.com>
2024-08-26 23:44:58 -0500

    Modified user controllers.ts

--- scr/controllers/user_controllers.ts
+++ scr/controllers/user_controllers.ts
@@ -4,17 +4,17 @@ import pool from "../database/db_connect";
 
 require('dotenv').config();
 
-export const generateToken = async (req: Request, res: Response): Promise<Response> =>{
+export const generateToken = async (req: Request, response: Response): Promise<Response> =>{
     const userName= req.body.username;
     const password = req.body.password;
-    const query = await pool.query('SELECT * FROM users WHERE user_name = $1 AND password = $2'
+    const query = await pool.query('SELECT * FROM users WHERE username = $1 AND password = $2'
         , [userName, password ]);
-    const user = query.rows[0];
+        const user = query.rows[0];
     if (query.rowCount !== null && query.rowCount > 0){
-        const accessToken = jwt.sign(user, `${process.env.CLAVE_JWT}`, {expiresIn: '1h'})
-        return res.status(200).json({accessToken})
+        const accessToken = jwt.sign(user, `${process.env.CLAVE_JWT}`, {expiresIn: '1h'});
+        return response.status(200).json({accessToken})
     }else {
-        return res.status(400).json('User not found')
+        return response.status(400).json('User not found')
     }
     
 };
